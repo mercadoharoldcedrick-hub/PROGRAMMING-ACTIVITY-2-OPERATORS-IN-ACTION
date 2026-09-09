1b3702538a190757ddf16565feb5970ed4547a96
mercadoharoldcedrick-hub <mercadoharoldcedrick@gmail.com>
2026-09-10 00:25:17 +0800

    Create act2_temp.py

---
+++ act2_temp.py
@@ -0,0 +1,10 @@
+# Name: Mercado, Harold Cedrick P.
+# Section: BMET - 2101
+# Task 4 - Temperature Check
+
+celsius = float(input("Enter temperature in °C: "))
+
+fahrenheit = celsius * 9 / 5 + 32
+
+print("Fahrenheit:", fahrenheit)
+print("Between 20 and 30 °C:", 20 <= celsius <= 30)
